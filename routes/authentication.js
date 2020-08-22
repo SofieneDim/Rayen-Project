793ba158710e79d44e520361fd03d4e9f2b9bb36
sofienedim <dimessisofiene@gmail.com>
2020-08-22 21:08:55 +0100

    auto push

---
+++ routes/authentication.js
@@ -0,0 +1,40 @@
+const express = require("express");
+router = express.Router();
+path = require('path');
+
+
+router.get("/signin", (req, res) => res.render("signin"));
+
+router.post("/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username == "admin") {
+        if (username == process.env.ADMIN_USERNAME && password == process.env.ADMIN_PASSWORD)
+            return res.redirect("/admin-dashboard");
+        else
+            return res.redirect("/");
+    } else {
+        const user = await User.findOne({ firstName: username });
+        if (username == user.firstName && password == user.password)
+            return res.send("Client connected successfully");
+        else
+            return res.redirect("/");
+    };
+});
+router.get("/signup", (req, res) => res.render("signup"));
+
+router.post("/signup", async (req, res) => {
+    const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+    });
+    try {
+        await newUser.save();
+        res.send("New user successfully registered!");
+    } catch (error) {
+        res.json({ message: error });
+    };
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,49 +12,19 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const usersRoute = require('./routes/users');
+app.use("/users", usersRoute);
+
+const authRoute = require('./routes/authentication');
+app.use("/auth", authRoute);
+
 // Routes
-app.get("/", (req, res) => res.redirect("/signin"));
-app.get("/signin", (req, res) => res.render("signin"));
-app.get("/signup", (req, res) => res.render("signup"));
+app.get("/", (req, res) => res.redirect("/auth/signin"));
 
 app.get("/admin-dashboard", async (req, res) => {
     const users = await User.find();
     res.render("adminDashboard", { users: users });
 });
-
-app.post("/signin", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (username == "admin") {
-        if (username == process.env.ADMIN_USERNAME && password == process.env.ADMIN_PASSWORD)
-            return res.redirect("admin-dashboard");
-        else
-            return res.redirect("/");
-    } else {
-        const user = await User.findOne({ firstName: username });
-        if (username == user.firstName && password == user.password)
-            return res.send("Client connected successfully");
-        else
-            return res.redirect("/");
-    };
-});
-
-app.post("/signup", async (req, res) => {
-    const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        password: req.body.password,
-    });
-    try {
-        await newUser.save();
-        res.send("New user successfully registered!");
-    } catch (error) {
-        res.json({ message: error });
-    };
-});
-
-const usersRoute = require('./routes/users');
-app.use("/users", usersRoute);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
